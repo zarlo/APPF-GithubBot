e90c80c041c6c8ea5e25402cbad545c69c89a811
zarlo <zarlo5899@gmail.com>
2018-03-16 12:11:29 +1100

    config

--- bot/bot.py
+++ bot/bot.py
@@ -8,5 +8,12 @@ if config['Token'] is not None:
     g = Github(config['Token'])
 else:
     g = Github(config['User'], config['Password'])
- 
-g.get_user(config['org']).get_repo(config['repo']).create_issue(sys.argv[1], sys.argv[2], GithubObject.NotSet, GithubObject.NotSet,  sys.argv[3].split(','), sys.argv[4].split(','))
+
+if config['org'] is not None:
+    user = g.get_user(config['User'])
+else:
+    user = g.get_organizationr(config['org'])
+
+repo = user.get_repo(config['repo'])
+
+repo.create_issue(sys.argv[1], sys.argv[2], GithubObject.NotSet, GithubObject.NotSet,  sys.argv[3].split(','), sys.argv[4].split(','))
